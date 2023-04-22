11c4c10027373e2ca9d44be0b3341f093153a761
Pedro Dias <pedro.afonso.cardoso.dias@gmail.com>
2023-04-22 02:20:57 +0100

    database_atualizado

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
         $servername = "localhost"; // or the IP address of your database server
-        $username = "admin"; // replace with your database username
+        $username = "id20641139_admin"; // replace with your database username
         $password = "AdminAdmin1!"; // replace with your database password
-        $dbname = "Data_Users"; // replace with your database name
+        $dbname = "id20641139_data_users	"; // replace with your database name
         
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
