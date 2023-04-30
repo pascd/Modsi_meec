430c1cb759b098809769edb5b4f980abf6d97029
Ninja-Spotted <ninjaoffirehd@gmail.com>
2023-04-30 21:16:08 +0100

    Atualização HTML Pag inicial

--- pagina_inicial/index.php
+++ pagina_inicial/index.php
@@ -11,38 +11,31 @@
     </head>
 
     <body href="#home">
-        <h1>
-            Sistema de vacinação Portuguesa!
-        </h1>
-
-        <script src="../jquery-3.6.4.min.js"></script>
-        <script> 
-            $(function(){
-            $("#menu_bar").load("../menu_bar.php"); 
-            });
-        </script>   
-
-        <div id="menu_bar"></div>
         <p>
             Neste site será possível agendar vacinações nos diferentes centros de saúde portugueses
         </p>
-        
-        <button>
-            Agendar Vacinação
-        </button>
+       
+        <div class="double_column">
+            <a href="cidadao.html">Cidadão</a>
+            <img src="Imagens/cidadao.png" alt="Imagem Cidadão"> 
+        </div>
+        <div class="double_column">
+            <a href="profissional.html">Profissional</a>
+            <img src="Imagens/profissional.png" alt="Imagem Profissional"> 
+        </div>
 
         <p>Fique a par das novidades!</p>
         <div class="newsfeed">
-            <div class="nf">
+            <div class="newsfeed_column">
                 <script src="//rss.bloople.net/?url=https%3A%2F%2Frssfeeds.webmd.com%2Frss%2Frss.aspx%3FRSSSource%3DRSS_PUBLIC&limit=1&showtitle=false&type=js"></script>            
             </div>
 
-            <div class="nf">
+            <div class="newsfeed_column">
                 <script src="//rss.bloople.net/?url=https%3A%2F%2Fwww.modernhealthcare.com%2Fsection%2Frss%2Fnews%3Fdays%3D7%26topics%3D81631&limit=1&showtitle=false&type=js"></script>
             </div>
 
             <!--https://www.dgs.pt/upload/DGSv9/rss/c5002.xml É ISTO QUE TEMOS EM PORTUGAL, UM RSS FEED VAZIO DGS-->
-            <div class="nf">
+            <div class="newsfeed_column">
                 <script src="//rss.bloople.net/?url=https%3A%2F%2Fwwwnc.cdc.gov%2Ftravel%2Frss%2Fnotices.xml&limit=1&showtitle=false&type=js"></script>
             </div>
         </div>
