166586cf266a938492afeea2e42e4795798dfc15
Ninja-Spotted <ninjaoffirehd@gmail.com>
2023-05-03 00:27:16 +0100

    Menu_bar e footer como ficheiros externos

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer" id="contactos">
+    <br>
+    <a class="legal t">© 2023 - 2023 Modelação e Simulação Industrial  | Porto, Portugal</a>
+    <br><br>
+    <div>
+        <a class="legal email" href="mailto:1181808@isep.ipp.pt" target="_blank">1181808@isep.ipp.pt</a>
+        <a class="legal email" href="mailto:1190937@isep.ipp.pt" target="_blank">1190937@isep.ipp.pt</a>
+        <a class="legal email" href="mailto:1190939@isep.ipp.pt" target="_blank">1190939@isep.ipp.pt</a>
+    </div>
+    
+    <a id="pt" href="?lang=pt" class="pt">Português</a>
+    <a id="en" href="?lang=en" class="en">English</a>
+    <br><br>
+</footer>
