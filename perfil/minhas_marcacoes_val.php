3718925845e8b792279a62373a32ebe3bcb0eea4
Pedro Dias <pedro.afonso.cardoso.dias@gmail.com>
2023-05-20 18:09:38 +0100

    Email_apagar

--- perfil/minhas_marcacoes_val.php
+++ perfil/minhas_marcacoes_val.php
@@ -11,7 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $acao = $_POST["acao"];
     echo var_dump($acao);
     $paciente = $_SESSION["id"];
-    $vaga_nova = $_POST["id_vaga_nova"];
 
     if ($acao == "Apagar") {
         $rem_sql = "DELETE FROM marcacao WHERE paciente='$paciente' AND vaga='$vaga'";
@@ -20,15 +19,37 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $sel_sql = "SELECT * FROM vagas WHERE id_vagas='$vaga'";
         $ans = mysqli_query($db, $sel_sql);
         $row = mysqli_fetch_array($ans);
+        $nome = $_SESSION['primeiro_nome'];
+        $apelido = $_SESSION['ultimo_nome'];
+        $data = $row['data_vaga'];
+        $hora = $row['hora'];
+        $vacina = $row['vacina'];
 
         $n_vagas = $row['vagas'] + 1;
 
         $query = "UPDATE vagas SET vagas='$n_vagas' WHERE id_vagas='$vaga'";
         mysqli_query($db, $query);
+
+        $html = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/mail/apagar_reserva_mail.html');
+
+        // Replace placeholders in HTML template with dynamic content
+        $emailContent = str_replace('{nome}', $nome, $html);
+        $emailContent = str_replace('{apelido}', $apelido, $emailContent);
+        $emailContent = str_replace('{vacina}', $vacina, $emailContent);
+        $emailContent = str_replace('{data}', $data, $emailContent);
+        $emailContent = str_replace('{hora}', $hora, $emailContent);
+
+        $destino = $_SESSION['email'];
+        $assunto = "Re-agendamento de Vacina";
+        $mensagem = $emailContent;
+
+        enviar($destino, $assunto, $mensagem);
+
     }
 
     if ($acao == "Alterar") {
         $estado = "Marcado";
+        $vaga_nova = $_POST["id_vaga_nova"];
 
         $ins_sql = "INSERT INTO marcacao(paciente, vaga, estado) VALUES ('$paciente', '$vaga_nova', '$estado')";
         mysqli_query($db, $ins_sql);
@@ -88,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $emailContent = str_replace('{hora_2}', $hora_2, $emailContent);
 
         $destino = $_SESSION['email'];
-        $assunto = "Agendamento de Vacina";
+        $assunto = "Re-agendamento de Vacina";
         $mensagem = $emailContent;
 
         enviar($destino, $assunto, $mensagem);
