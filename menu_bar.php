3ad94c4fc714c6baf22b9e0c45e9368f003edeb4
Pingtj <18302paulosilva@gmail.com>
2023-06-02 20:48:59 +0100

    menu-bar

--- menu_bar.php
+++ menu_bar.php
@@ -91,16 +91,19 @@
                                     <li class="nav-item active">
                                         <a class="nav-link" href="/FrontEnd/index.php">Home <span class="sr-only">(current)</span></a>
                                     </li>
-                                    <li class="nav-item dropdown">
+                                    <!-- <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acerca</a>
                                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                             <a class="dropdown-item" href="/acerca/missao_e_valores.html">Missão e Valores</a>
                                             <a class="dropdown-item" href="/acerca/welcome.html">Mensagem de Boas Vindas</a>
                                             <a class="dropdown-item" href="/acerca/contactos.html">Contactos</a>
                                         </div>
+                                    </li> -->
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/acerca/contactos.html">Contactos</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="/publicacoes.html">Publicações</a>
+                                        <a class="nav-link" href="/acerca/missao_e_valores.html">Missão & Valores</a>
                                     </li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Saúde Pública</a>
